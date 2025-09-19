ff908f51d501e01367f50c044da306e5c967471f
Baryczka25 <gusedubary21@gmail.com>
2025-09-19 11:04:46 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,7 @@ public class Main {
             JFrame frame = new JFrame("Jogo Clicker");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setSize(400, 300);
-
-            // Corrigindo: criando um JLabel
+            
             JLabel lbl = new JLabel("Bem-vindo ao Joguinho!");
             lbl.setFont(new Font("Arial", Font.BOLD, 16));
             lbl.setHorizontalAlignment(SwingConstants.CENTER);
